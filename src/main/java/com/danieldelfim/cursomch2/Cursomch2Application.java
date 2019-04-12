6e94282b16bdb6ec2b97e1292d0ba28a86b0cb1b
Daniel Delfim <dmdelfim@gmail.com>
2019-04-12 07:55:20 -0300

    Created project

---
+++ src/main/java/com/danieldelfim/cursomch2/Cursomch2Application.java
@@ -0,0 +1,13 @@
+package com.danieldelfim.cursomch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomch2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomch2Application.class, args);
+	}
+
+}
