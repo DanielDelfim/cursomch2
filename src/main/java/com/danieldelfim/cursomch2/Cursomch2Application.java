2cccf746c6e2b2b52ce5f1dc079aea6caf3b2d17
Daniel Delfim <dmdelfim@gmail.com>
2019-04-13 10:02:34 -0300

    Cities and states created

--- src/main/java/com/danieldelfim/cursomch2/Cursomch2Application.java
+++ src/main/java/com/danieldelfim/cursomch2/Cursomch2Application.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.danieldelfim.cursomch2.domain.Category;
+import com.danieldelfim.cursomch2.domain.Cidade;
+import com.danieldelfim.cursomch2.domain.Estado;
 import com.danieldelfim.cursomch2.domain.Product;
 import com.danieldelfim.cursomch2.repositories.CategoryRepository;
+import com.danieldelfim.cursomch2.repositories.CidadeRepository;
+import com.danieldelfim.cursomch2.repositories.EstadoRepository;
 import com.danieldelfim.cursomch2.repositories.ProductRepository;
 
 @SpringBootApplication
@@ -21,6 +25,11 @@ public class Cursomch2Application implements CommandLineRunner {
 	CategoryRepository categoryRepository;
 	@Autowired // Instancia automaticamente o objeto productRepository e executamos o método necessário 
 	ProductRepository productRepository;
+	@Autowired // Instancia automaticamente o objeto categoryRepository e executamos o método necessário 
+	CidadeRepository cidadeRepository;
+	@Autowired // Instancia automaticamente o objeto productRepository e executamos o método necessário 
+	EstadoRepository estadoRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomch2Application.class, args);
@@ -49,6 +58,21 @@ public class Cursomch2Application implements CommandLineRunner {
 //finalizada a tabela de associação PRODUCT_CATEGORY
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+//		declamos os estados
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+//		Agora cada cidade já conhece seu estado
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+//		now states know every cities in your list
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
 // com o categoryRepository. temos acesso a todas os métodos disponíveis no objeto. Como são várias caegorias, salvamos como lista.
 		
 	}
